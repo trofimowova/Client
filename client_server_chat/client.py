6b1d2a7b14028ad07b36f502a5769612308ce037
trofimowova <trofimowova@yandex.ru>
2021-05-05 06:33:54 -0700

    Threading

--- client_server_chat/client.py
+++ client_server_chat/client.py
@@ -1,4 +1,5 @@
 import socket
+from threading import Thread
 
 client = socket.socket(
     socket.AF_INET,
@@ -8,7 +9,24 @@ client = socket.socket(
 client.connect(
     ("127.0.0.1",1234)
 )
-while True:
-    data = client.recv(2048)
-    print(data.decode('utf-8'))
-    client.send(input(':::').encode("utf-8"))
+
+
+
+def listen_server():
+    while True:
+        data = client.recv(2048)
+        print(data.decode('utf-8'))
+
+
+def send_server():
+    listen_thread = Thread(target=listen_server)#---------------------one thread listens
+    listen_thread.start()#---------------------------inevitably  start!
+
+    while True:
+        client.send(input('...').encode("utf-8"))#--------------------second one sends
+
+
+
+
+if __name__ == "__main__":
+    send_server()
