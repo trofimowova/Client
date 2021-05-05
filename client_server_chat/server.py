2aacd848a298844826c04746b4d72d5262f7e472
trofimowova <trofimowova@yandex.ru>
2021-05-05 12:21:29 -0700

    Asyncio server and class Socket

--- client_server_chat/server.py
+++ client_server_chat/server.py
@@ -1,46 +1,45 @@
 from Socket import Socket
-import threading
+import asyncio
 
 class Server(Socket):
     def __init__(self):
         super(Server,self).__init__()
-        #self.listen(5) #---can accept
-        print("Server is listening")
         self.users =[]
 
     def set_up(self):
-        self.bind(("127.0.0.1",8000))
-        self.listen(5)
-        self.accept_sockets()
+        self.socket.bind(("127.0.0.1",8000))
+        self.socket.listen(5)
+        self.socket.setblocking(False)# socket works always/ unblocking condition
+        print("Server is listening")
 
-    def send_data(self,data):
+    async def send_data(self,data):
         for user in self.users:
-            user.send(data) 
+            await self.main_loop.sock_sendall(user,data)
 
-    def listen_socket(self,listened_socket=None):
+    async def listen_socket(self,listened_socket=None):
         print("Listening user...")
+        if not listened_socket:
+            return
 
         while True:
-            data = listened_socket.recv(2048)
+            data = self.main_loop.sock_recv(listened_socket,2048)
             print(f"User send{data}")
-            self.send_data(data)
+            await self.send_data(data)
             
 
-    def accept_sockets(self):
+    async def accept_sockets(self):
         while True:
-            user_socket, adress = self.accept()#--------------------accepts tuple(socket,adress)
+            user_socket, adress = await self.main_loop.sock_accept(self.socket)#--------------------accepts tuple(socket,adress)
             print(f"User {adress[0]} connected")
             
             self.users.append(user_socket)
+            self.main_loop.create_task(self.listen_socket(user_socket))
 
-            listen_accepted_user=threading.Thread(
-                target=self.listen_socket,
-                args=(user_socket,)
-                )
-            listen_accepted_user.start()
-        
+    async def main(self):
+        await self.main_loop.create_task(self.accept_sockets())
 
     
 if __name__ == "__main__":
     server = Server()
     server.set_up()
+    server.start_loop()
