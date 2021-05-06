a179a261ceea454d653eb1dc37ebe36d54496442
trofimowova <trofimowova@yandex.ru>
2021-05-06 13:47:34 -0700

    send_data , start_up function

--- jim/client.py
+++ jim/client.py
@@ -9,19 +9,24 @@ client = socket.socket(
 client.connect(
     ("127.0.0.1",1234)
 )
-while True:
+def start_up():   
     data = client.recv(2048)
     print(data.decode('utf-8'))
-    
-    msg = {
-    "action": "msg",
-    "time": "<unix timestamp>",
-    "to": "#room_name",
-    "from": "account_name",
-     "message": "Hello World"
-    }
-        
-    client.send(pickle.dumps(msg))
-    
+    send_data()
 
+def send_data():
+    while True:
+        msg = {
+        "action": "msg",
+        "time": "<unix timestamp>",
+        "to": "#room_name",
+        "from": "account_name",
+        "message": "Hello World"
+        }
+            
+        client.send(pickle.dumps(msg))
+        
+
+if __name__=="__main__":
+    start_up()
 
